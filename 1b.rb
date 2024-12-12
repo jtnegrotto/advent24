81b3a4d1d639e87deb154fcdabda77154015a1fc
Julien Negrotto <jtnegrotto@gmail.com>
2024-12-12 13:49:21 -0600

    Pare down 1B

--- 1b.rb
+++ 1b.rb
@@ -1,11 +1,4 @@
-puts ARGF
-  .each_line
-  .map(&:strip).reject(&:empty?)     # clean up input
-  .flat_map(&:split).map(&:to_i)     # split lines and parse to ints
-  .partition.with_index { _2.even? } # partition back into two lists
-  .then {
-    t = _2.tally                     # tally the second list
-    _1.map { |n| n * (t[n] || 0) }   # get similarity score
-  }
-  .sum
+$><<$<.read.split.map(&:to_i)            # Read, split, parse to int array
+  .each_slice(2).to_a.transpose          # Columnize pairs
+  .then{|a,b|a.sum{_1*(b.tally[_1]||0)}} # Sum similarity scores
 
