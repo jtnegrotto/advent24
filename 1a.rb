1f7a85fdda5cbd4a1a88f72290936e6dc69adc8f
Julien Negrotto <jtnegrotto@gmail.com>
2024-12-12 12:33:44 -0600

    Pare down 1A solution

--- 1a.rb
+++ 1a.rb
@@ -1,9 +1,3 @@
-puts ARGF
-  .each_line
-  .map(&:strip).reject(&:empty?)     # clean up input
-  .flat_map(&:split).map(&:to_i)     # split lines and parse to ints
-  .partition.with_index { _2.even? } # partition back into two lists
-  .map(&:sort)                       # sort both lists
-  .inject(:zip)                      # zip sorted lists together
-  .map { _1.inject(:-).abs }         # get abs diffs of each pair
-  .sum
+$><<$<.read.split.map(&:to_i)               # Read, split, parse to int array
+  .each_slice(2).to_a.transpose.map(&:sort) # Pair, columnize, sort ints
+  .reduce(:zip).sum{(_1-_2).abs}            # Pair columns, diff, sum
